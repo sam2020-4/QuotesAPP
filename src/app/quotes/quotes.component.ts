295b26609407061eab818347e559f372e588ff97
matata samuel <mattasamuel3@gmail.com>
2020-08-22 13:33:01 +0300

    inserted new contented on quotes component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  // updating output binding property
+  quotes: Quote[] = [
+    new Quote(1, 'Life Quote', 'Keep looking up thats the secret of life', 'matata samuel', 'Hancel Asher', new Date(2020, 7, 23)),
+    new Quote(2, 'Social Quote', 'Those who realize their folly are not true fools', 'Victor Manuel','Tony wein', new Date(2015, 4, 11))
+  ];
+
+  
+
+
 
   constructor() { }
 
