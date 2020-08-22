f0fc670eefe9c50d66a74a81693d98a292e64712
matata samuel <mattasamuel3@gmail.com>
2020-08-22 13:45:54 +0300

    inserted new contented on quotes-details component

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
