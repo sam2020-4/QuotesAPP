3ca1ec581dec27eda60b00854125bdb8faecb415
matata samuel <mattasamuel3@gmail.com>
2020-08-22 12:56:21 +0300

    insert Quotes component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+
+    showDescription: boolean;
+
+    constructor(
+        public id: number,
+        public name: string,
+        public description: string,
+        public author: string,
+        public submittedby: string,        
+        public completeDate: Date) 
+    { 
+        this.showDescription = false;
+     }
 }
